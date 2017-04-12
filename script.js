703c1d8e4420744fcee6d51e8efc2e605d0eed69
Daniel Paten <Daniel@Daniels-MacBook-Pro.local>
2017-04-12 15:55:45 +1000

    Added search page and location option

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-id_searchBar = 'searchBar';
-id_typeSelector= 'typeSelector';
-id_suburbSelector = 'suburbSelector';
-id_ratingSelector = 'ratingSelector';
+name_searchBar = 'searchBar';
+name_typeSelector= 'typeSelector';
+name_suburbSelector = 'suburbSelector';
+name_ratingSelector = 'ratingSelector';
 
 // the file path
 filledStar = 'imgs/filledStar.svg';
@@ -47,56 +47,101 @@ function showMapMarkers()
  */
 function typeSelectorChanged()
 {
-	var type = document.getElementById(id_typeSelector).value;
+    var type = document.getElementsByName(name_typeSelector)[0].value;
+    
+    
+    // reset rating stars in the search box
+    resetRatingStars(document.getElementsByName(name_ratingSelector)[0]);
 
-	// reset rating stars in the search box
-	resetRatingStars(document.getElementById(id_ratingSelector));
-
-	if (type == 'suburb') {
-		showSuburbSelector();
-		hideRatingSelector();
-		hideSearchBar();
-
-	} else if (type == 'rating') {
-		showRatingSelector();
-		hideSuburbSelector();
-		hideSearchBar();
-
-	} else if (type == 'name') {
-		showSearchBar();
-		hideSuburbSelector();
-		hideRatingSelector();
-	}
+    if (type == 'name') {
+        showSearchBar();
+        hideSuburbSelector();
+        hideRatingSelector();
+        
+    } else if (type == 'suburb') {
+        showSuburbSelector();
+        hideRatingSelector();
+        hideSearchBar();
+        
+    } else if (type == 'rating') {
+        showRatingSelector();
+        hideSuburbSelector();
+        hideSearchBar();
+        
+    } else if (type == 'location') {
+        showSearchBar();
+        hideSuburbSelector();
+        hideRatingSelector();
+        getLocation();
+    }
 }
 
 function showSuburbSelector()
 {
-	document.getElementById(id_suburbSelector).style.visibility = 'visible';
+    document.getElementsByName(name_suburbSelector)[0].style.display = 'initial';
 }
 
 function hideSuburbSelector()
 {
-	document.getElementById(id_suburbSelector).style.visibility = 'hidden';
+    document.getElementsByName(name_suburbSelector)[0].style.display = 'none';
 }
 
 function showRatingSelector()
 {
-	document.getElementById(id_ratingSelector).style.visibility = 'visible';
+    document.getElementsByName(name_ratingSelector)[0].style.display = 'initial';
 }
 
 function hideRatingSelector()
 {
-	document.getElementById(id_ratingSelector).style.visibility = 'hidden';
+    document.getElementsByName(name_ratingSelector)[0].style.display = 'none';
 }
 
 function showSearchBar()
 {
-	document.getElementById(id_searchBar).style.visibility = 'visible';
+    document.getElementsByName(name_searchBar)[0].style.display = 'initial';
+    document.getElementsByName(name_searchBar)[0].placeholder = ' Search parks';
 }
 
 function hideSearchBar()
 {
-	document.getElementById(id_searchBar).style.visibility = 'hidden';
+    document.getElementsByName(name_searchBar)[0].style.display = 'none';
+}
+
+// These functions get the user's current location and puts it in the search box automatically.
+// If the user cannot do geolocation the appropriate message is shown.
+function getLocation() {
+    var searchBox = document.getElementsByName(name_searchBar)[0];
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition, showError);
+    } else {
+        searchBox.innerHTML = " Geolocation is not supported by your browser.";
+    }
+}
+
+function showPosition(position) {
+    var searchBox = document.getElementsByName(name_searchBar)[0];
+    searchBox.placeholder = position.coords.latitude + ", " + position.coords.longitude;
+}
+
+
+function showError(error) {
+    var searchBox = document.getElementsByName(name_searchBar)[0];
+    var msg = "";
+    switch(error.code) {
+        case error.PERMISSION_DENIED:
+            msg = " Denied the request for Geolocation."
+            break;
+        case error.POSITION_UNAVAILABLE:
+            msg = " Location information is unavailable."
+            break;
+        case error.TIMEOUT:
+            msg = " The request to get your location timed out."
+            break;
+        case error.UNKNOWN_ERROR:
+            msg = " An unknown error occurred."
+            break;
+    }
+    searchBox.placeholder = msg;
 }
 
 /* when a unfilled rating star with a index is clicked, all stars before
@@ -107,18 +152,18 @@ function hideSearchBar()
  */
 function ratingStarClicked(ratingImg, index)
 {
-	var parent = ratingImg.parentElement;
-	var children = parent.children;
-
-	resetRatingStars(parent);
-
-	for (var i = 0; i < index; i++) {
-		children[i].src = filledStar;	
-	}
-
-	// restore the rating to its parent node, which will be sent to the server
-	// when users click submit button.
-	parent.value = index;
+    var parent = ratingImg.parentElement;
+    var children = parent.children;
+    
+    resetRatingStars(parent);
+    
+    for (var i = 0; i < index; i++) {
+        children[i].src = filledStar;
+    }
+    
+    // restore the rating to its parent node, which will be sent to the server
+    // when users click submit button.
+    parent.value = index;
 }
 
 /* reset stars to default
@@ -127,11 +172,11 @@ function ratingStarClicked(ratingImg, index)
  */
 function resetRatingStars(parent)
 {
-	for (var i = 0; i < parent.children.length; i++) {
-		parent.children[i].src = unfilledStar;	
-	}
-
-	parent.value = 0;
+    for (var i = 0; i < parent.children.length; i++) {
+        parent.children[i].src = unfilledStar;
+    }
+    
+    parent.value = 0;
 }
 
 /* simplify the interface between client side and server side
@@ -142,22 +187,22 @@ function resetRatingStars(parent)
  */
 function searchSubmit(form)
 {
-	var ratingSelector = document.getElementById(id_ratingSelector);
-	var suburbSelector = document.getElementById(id_suburbSelector);
-	var searchBar = document.getElementById(id_searchBar);
-	var type = document.getElementById(id_typeSelector).value;
-
-	if (type == 'suburb') {
-		searchBar.value = suburbSelector.value;
-
-	} else if (type == 'rating') {
-		searchBar.value = ratingSelector.value; 
-	}
-
-	// remove these elements from form so that the data from them
-	// won't be sent
-	form.removeChild(ratingSelector);
-	form.removeChild(suburbSelector);
-
-	return true;
+    var ratingSelector = document.getElementsByName(name_ratingSelector)[0];
+    var suburbSelector = document.getElementsByName(name_suburbSelector)[0];
+    var searchBar = document.getElementsByName(name_searchBar)[0];
+    var type = document.getElementsByName(name_typeSelector)[0].value;
+    
+    if (type == 'suburb') {
+        searchBar.value = suburbSelector.value;
+        
+    } else if (type == 'rating') {
+        searchBar.value = ratingSelector.value; 
+    }
+    
+    // remove these elements from form so that the data from them
+    // won't be sent
+    form.removeChild(ratingSelector);
+    form.removeChild(suburbSelector);
+    
+    return true;
 }
